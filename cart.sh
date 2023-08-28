97c9369310481bebcda35d931b7ff5fc390dda80
harikapyneni <snehapyneni@gmail.com>
2023-08-28 10:51:02 -0400

    added cart

--- cart.sh
+++ cart.sh
@@ -35,3 +35,70 @@ VALIDATE()
         echo -e "$2...$G SUCCESS $N"
     fi
 }
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+USER_ROBOSHOP=$(id roboshop)
+
+if [ $? -ne 0 ];
+then
+    echo -e "$Y...USER roboshop is not present so creating now..$N"
+    useradd roboshop &>>$LOGFILE
+else
+    echo -e "$G...USER roboshop is already present so skipping now.$N"
+fi
+
+#checking the user app directory 
+#write a condition to check directory already exist or not
+
+VALIDATE_APP_DIR=$(cd /app)
+{
+if [ $? -ne 0 ];
+then
+    echo -e " $Y /app directory not there so creating now $N"
+    mkdir /app &>>$LOGFILE  
+else
+    echo -e "$G /app directory already present so skipping now $N"
+fi
+}
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/cart.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping cart"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+# give full path of cart.service because we are inside /app
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
+
+VALIDATE $? "copying cart.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable cart &>>$LOGFILE
+
+VALIDATE $? "Enabling cart"
+
+systemctl start cart &>>$LOGFILE
+
+VALIDATE $? "Starting cart"
