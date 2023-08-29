8f0614c95f11ddb87d46dbc101ba711a927c6696
harikapyneni <snehapyneni@gmail.com>
2023-08-29 19:12:49 -0400

    modified

--- catalogue.sh
+++ catalogue.sh
@@ -35,34 +35,6 @@ VALIDATE()
         echo -e "$2...$G SUCCESS $N"
     fi
 }
-#!/bin/bash
-
-DATE=$(date +%F)
-LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
-
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
-fi
-
-VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
 
 #create catalogue.service file in vscode seperately and copy vim code in roboshop documentation
 #give validations for each ie copied & pasted from catalogue.md in roboshop documentation
