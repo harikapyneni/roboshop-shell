71c91108f3656d5dab5ff31edbb3cfabf1aecc08
harikapyneni <snehapyneni@gmail.com>
2023-08-29 20:07:13 -0400

    modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,7 +3,7 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+SECURITY_GROUP_ID=sg-0e80af9278ee7cc87
 DOMAIN_NAME=ops2ai.cloud
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
@@ -17,10 +17,14 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
+
+# jq -- jason query , && -- and , || -- or    
+    
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+   
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z05806891LJZ0TR0VRIAI --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
